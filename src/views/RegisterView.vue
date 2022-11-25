0815a77033e546b90c8df927fb7a100d835e0760
Bricute322 <jbeugenio34@gmail.com>
2022-11-26 00:02:57 +0800

    RegisterView and Register Comp.

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,21 @@
+<template>
+  <register2 />
+</template>
+
+<script>
+import register2 from "../components/register2";
+
+export default {
+  name: "RegisterView",
+
+  components: {
+    register2,
+  },
+};
+</script>
+<!-- <style scoped>
+.test {
+  max-height: 100;
+  /* background-color: red; */
+}
+</style> -->
