54d534c8b731dfa6ed91d039311a36219d895896
Bricute322 <jbeugenio34@gmail.com>
2022-11-26 00:01:33 +0800

    LoginView, and Login

---
+++ src/views/LoginView.vue
@@ -0,0 +1,21 @@
+<template>
+  <Login />
+</template>
+
+<script>
+import Login from "../components/Login";
+
+export default {
+  name: "LoginView",
+
+  components: {
+    Login,
+  },
+};
+</script>
+<style scoped>
+.test {
+  max-height: 100;
+  /* background-color: red; */
+}
+</style>
