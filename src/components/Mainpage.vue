9051f1cc3166c34d91dd63e9516937c06336d405
Bricute322 <jbeugenio34@gmail.com>
2022-11-26 00:05:09 +0800

    MainpageView and Mainpage Comp.

---
+++ src/components/Mainpage.vue
@@ -0,0 +1,47 @@
+<template>
+  <!-- <v-toolbar prominent class="flex-grow-0 test">
+    <v-toolbar-title>
+      <v-row no-gutters>
+        <v-img contain height="100" width="100px" v-bind:src="logO"></v-img>
+      </v-row>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn icon>
+        <v-icon>mdi-bell-outline</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-message-outline</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-account-outline</v-icon>
+      </v-btn></v-toolbar-items
+    >
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn icon>
+        <v-icon>mdi-bell-outline</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-message-outline</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-account-outline</v-icon>
+      </v-btn></v-toolbar-items
+    >
+  </v-toolbar> -->
+  <Navigation
+    ><container><h1>asd</h1></container>></Navigation
+  >
+</template>
+<script>
+import Navigation from "@/components/Layout/Navigation.vue";
+export default {
+  name: "Mainpage",
+  data: () => ({}),
+  components: {
+    Navigation,
+  },
+};
+</script>
+<style scoped></style>
---
+++ src/views/MainPageView.vue
@@ -0,0 +1,21 @@
+<template>
+  <Mainpage />
+</template>
+
+<script>
+import Mainpage from "../components/Mainpage";
+
+export default {
+  name: "MainpageView",
+
+  components: {
+    Mainpage,
+  },
+};
+</script>
+<style scoped>
+.test {
+  max-height: 100;
+  /* background-color: red; */
+}
+</style>
